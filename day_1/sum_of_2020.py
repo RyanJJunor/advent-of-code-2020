ff78d546370b7a786c3a60a1da7fbe40c3ef6449
Ryan Junor <ryanjjunor@gmail.com>
2020-12-01 21:03:43 +0000

    Completed day 1 Included two solution for both part one and part two The two functions for task one take very similar times. For part two, the second solution out performs the first with the first taking ~0.2 seconds and the second taking ~0.03 seconds

--- day_1/sum_of_2020.py
+++ day_1/sum_of_2020.py
@@ -1,2 +1,44 @@
+import time
+
 f = open("expense_report", "r")
-print(f.read())
+
+list = []
+for x in f:
+    list.append(int(x))
+
+
+def find_multiple_value(list):
+    for x in list:
+        for y in list:
+            if (x + y) == 2020:
+                return x * y
+
+
+def find_multiple_value_other(list):
+    for x in list:
+        if (2020 - x) in list:
+            return x * list[list.index(2020 - x)]
+
+
+def find_multiple_value_part_2(list):
+    for x in list:
+        for y in list:
+            for z in list:
+                if (x + y + z) == 2020:
+                    return x * y * z
+
+
+def find_multiple_value_other_part_2(list):
+    for x in list:
+        for y in list:
+            if (2020 - x - y) in list:
+                return x * y * list[list.index(2020 - x - y)]
+
+
+start_time = time.time()
+print(find_multiple_value_part_2(list))
+print("--- first function took %s seconds ---" % (time.time() - start_time))
+
+start_time = time.time()
+print(find_multiple_value_other_part_2(list))
+print("--- second function took %s seconds ---" % (time.time() - start_time))
