f2d9fcb261af1bfe47777fdfe6846633a1eb583a
Ryan Junor <ryanjjunor@gmail.com>
2020-12-03 22:41:55 +0000

    Added a different way of handling going too far right off the pattern by using modulo to go to the appropriate index at the beginning of the pattern

--- day_3/number_of_trees.py
+++ day_3/number_of_trees.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 f = open("map", "r").read().splitlines()
 
@@ -32,11 +33,42 @@ def part_one(list, down, right):
 
 
 def part_two():
-
     answer = part_one(list, 1, 1) * part_one(list, 1, 3) * part_one(list, 1, 5) * part_one(list, 1, 7) * part_one(list,
                                                                                                                   2, 1)
     return answer
 
 
-# print(part_one(list, 1, 3))
+def part_one_2(list, down, right):
+    length = (len(list[0]))
+
+    iterations = int(math.floor(len(list) / down))
+
+    trees = 0
+    count_right = 0
+    count_down = 0
+
+    for i in range(down, iterations):
+        count_right = count_right + right
+        count_down = count_down + down
+
+        if count_right >= length:
+            count_right = (count_right % length)
+
+        if list[count_down][count_right] == "#":
+            trees = trees + 1
+
+    return trees
+
+
+def part_two_2():
+    answer = part_one_2(list, 1, 1) * part_one_2(list, 1, 3) * part_one_2(list, 1, 5) * part_one_2(list, 1,
+                                                                                                   7) * part_one_2(list,
+                                                                                                                   2, 1)
+    return answer
+
+
+print(part_one(list, 1, 3))
 print(part_two())
+
+print(part_one_2(list, 1, 3))
+print(part_two_2())
