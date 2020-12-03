956a02f2e2708e77a08127919d43a8113961a0a3
Ryan Junor <ryanjjunor@gmail.com>
2020-12-03 21:50:03 +0000

    Completed day 3 Quite happy with my solution

---
+++ day_3/number_of_trees.py
@@ -0,0 +1,42 @@
+import math
+
+f = open("map", "r").read().splitlines()
+
+list = []
+for x in f:
+    list.append(x)
+
+
+def part_one(list, down, right):
+    length = (len(list[0]))
+
+    iterations = int(math.floor(len(list) / down))
+
+    multiples_needed = math.ceil(((iterations - 1) * right) / length)
+
+    new_map = []
+    for i in list:
+        new_map.append(i * multiples_needed)
+
+    trees = 0
+    count_right = 0
+    count_down = 0
+
+    for i in range(down, iterations):
+        count_right = count_right + right
+        count_down = count_down + down
+        if new_map[count_down][count_right] == "#":
+            trees = trees + 1
+
+    return trees
+
+
+def part_two():
+
+    answer = part_one(list, 1, 1) * part_one(list, 1, 3) * part_one(list, 1, 5) * part_one(list, 1, 7) * part_one(list,
+                                                                                                                  2, 1)
+    return answer
+
+
+# print(part_one(list, 1, 3))
+print(part_two())
